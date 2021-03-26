f3a67bced31205d7871e99785bd52cf7ebe11e3d
Cherron Simes <simes@CIM-LAPTOP-SIMES.CIMCOR.COM>
2021-03-25 19:39:30 -0500

    Initialized class_notes repo

---
+++ day_2_notes.js
@@ -0,0 +1,14 @@
+// Git
+// Version control system designed to keep chronological timeline of all of your code changes.
+// To traverse file system in terminal:
+// cd name_of_folder --> moves you down
+// cd .. --> moves you up
+// ls --> lists items inside existing folder
+// pwd --> specifies directory path you're in (print working directory)
+
+// To initiate a git repository
+// git init
+// Staging files for a commit
+// git status (tells status of your repo)
+// git add <file_name> OR git add . to add all of the files inside the folder that you are in and stage them for a commit
+//git commit -m "Your commit message here"
