41b4567386bead47692dac5f0d9ca8e0fd145f27
Cherron Simes <cherron@developdbycherron.com>
2021-03-25 19:56:57 -0500

    Added more git info

--- day_2_notes.js
+++ day_2_notes.js
@@ -11,4 +11,9 @@
 // Staging files for a commit
 // git status (tells status of your repo)
 // git add <file_name> OR git add . to add all of the files inside the folder that you are in and stage them for a commit
-//git commit -m "Your commit message here"
+// To commit your changes, run the following command:
+// git commit -m "Your commit message here"
+
+// To view the commit: 
+// git log
+// Shows us commit hash, branch name, commit message, and other metadata
