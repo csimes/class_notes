2c1f77831da6638aeb258f07ec47d140321675e0
Cherron Simes <cherron@developdbycherron.com>
2021-05-08 09:49:21 -0500

    Array Destructuring lecture

--- arr_destructuring.js
+++ arr_destructuring.js
@@ -20,24 +20,24 @@ let unpackedFullName = [...fullName];
 // let editedFullName = [fullName];
 // console.log(editedFullName);
 
-fullName.unshift("Mr.")
+// fullName.unshift("Mr.")
 
-console.log(fullName, unpackedFullName);
+// console.log(fullName, unpackedFullName);
 
 // console.log(Math.min(1, 5, -3));
 
 let prices = [10.99, 5.99, 22.99, 6.88]
 // console.log(Math.min(prices));
 // console.log(typeof prices);
-console.log(Math.min(prices));// errors out because Math.min requires integers to function
-console.log(Math.min(...prices)); // unpacks our array values
+// console.log(Math.min(prices));// errors out because Math.min requires integers to function
+// console.log(Math.min(...prices)); // unpacks our array values
 
 // Primitive data types stored inside memory as a variable and a value
 let x = 10;
 let y = "abc";
 let a = x;
 let b = y; 
-console.log(x, y, a, b);
+// console.log(x, y, a, b);
 
 // JS has 3 data types that are objects which reference to a value:
 /*
@@ -46,4 +46,85 @@ console.log(x, y, a, b);
     - object    
 */
 
-let arr = [ ]
+let arr = [ ];
+// console.log(arr);
+
+arr.push(1);
+// console.log(arr);
+
+let reference = [1];
+let refCopy = reference;
+// console.log(refCopy);
+
+let persons = [{name: "Zach", age: 28}, {name: "Donovan", age: 30}];
+
+let copiedPersons = [...persons];
+
+// console.log(persons);
+// console.log("-------------");
+// console.log(copiedPersons);
+
+persons.push({name: "Anna", age: 29});
+
+// console.log(persons);
+// console.log("-------------");
+// console.log(copiedPersons);
+
+copiedPersons[0].name = "Zachary";
+persons[0].name = "Michael";
+
+// console.log(persons);
+// console.log("-------------");
+// console.log(copiedPersons);
+
+/*
+The return of the commands above returns Michael in persons and copiedPersons array. This happens because we copied the address to the place in memory with the copiedPersons variable we created. We then changed the value of our name key in the memory itself. Any changes we make to either of the arrays will be reflected in its counterpart.
+ */
+
+let mappedPersons = persons.map (person => ({
+    name: person.name,
+    age: person.age
+}));
+mappedPersons[0].name = "Zachary";
+
+// console.log(persons);
+// console.log("-------------");
+// console.log(mappedPersons);
+
+/*
+Only copy what you plan on changing, and where you don't want those changes reflected in the original set of data.
+*/
+
+// Array Destructuring Continued
+
+// let fullName = ["Paul", "Niemcyzk"];
+
+// let firstName = fullName[0];
+
+// console.log(firstName);
+
+let [firstName, lastName] = fullName;
+
+console.log(firstName);
+console.log(lastName);
+
+/*
+JS pulls values from fullName and assigns them based off their position to the variables we declared using destructuring.
+ */
+
+// Rest Syntax
+
+/*
+Utilizes the spread operator (...) to condense values into a single element rather than unpacking them.
+    - Any values left over from the original object will be packaged inside of it.
+    - Values can be skipped before packing them, by utilizing commas (,,,...myObject). 
+    - Rest operator must be the last item of destructured array syntax.
+*/
+
+let restArray = ["Paul", "Niemcyzk", "Omit 1", {month: 6, day: 15, year: 1990}, "Omit 2", {sport: "volleyball"}, "Omit 3"];
+
+let [firstName, lastName, , , ,...myObject] = restArray;
+
+console.log(firstName);
+console.log(lastName);
+console.log(myObject);
