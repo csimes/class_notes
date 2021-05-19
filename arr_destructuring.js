71ee60143fd96f182237902135e6c16cd5c76e4f
Cherron Simes <cherron@developdbycherron.com>
2021-05-19 17:58:38 -0500

    Update to classes and array destructuring to include repl review

--- arr_destructuring.js
+++ arr_destructuring.js
@@ -1,130 +1,172 @@
-// Array Destructuring & Spread Operator
-/*
-Ability to unpack values from arrays, or properties from objects into distinct variables while using the spread operator (...)
+// // Array Destructuring & Spread Operator
+// /*
+// Ability to unpack values from arrays, or properties from objects into distinct variables while using the spread operator (...)
 
-Spread operator pulls out all array elements and returns as a standalone list of elements.
-*/
-
-let fullName = ["Paul", "Niemcyzk"];
-
-let unpackedFullName = [...fullName];
-
-// for (name of fullName) {
-//     let nameNew = []
-//     nameNew.push(name)
-//     return nameNew
-// }
-
-// console.log(unpackedFullName);
-
-// let editedFullName = [fullName];
-// console.log(editedFullName);
-
-// fullName.unshift("Mr.")
-
-// console.log(fullName, unpackedFullName);
-
-// console.log(Math.min(1, 5, -3));
-
-let prices = [10.99, 5.99, 22.99, 6.88]
-// console.log(Math.min(prices));
-// console.log(typeof prices);
-// console.log(Math.min(prices));// errors out because Math.min requires integers to function
-// console.log(Math.min(...prices)); // unpacks our array values
-
-// Primitive data types stored inside memory as a variable and a value
-let x = 10;
-let y = "abc";
-let a = x;
-let b = y; 
-// console.log(x, y, a, b);
-
-// JS has 3 data types that are objects which reference to a value:
-/*
-    - array
-    - function
-    - object    
-*/
-
-let arr = [ ];
-// console.log(arr);
-
-arr.push(1);
-// console.log(arr);
-
-let reference = [1];
-let refCopy = reference;
-// console.log(refCopy);
-
-let persons = [{name: "Zach", age: 28}, {name: "Donovan", age: 30}];
-
-let copiedPersons = [...persons];
-
-// console.log(persons);
-// console.log("-------------");
-// console.log(copiedPersons);
-
-persons.push({name: "Anna", age: 29});
-
-// console.log(persons);
-// console.log("-------------");
-// console.log(copiedPersons);
-
-copiedPersons[0].name = "Zachary";
-persons[0].name = "Michael";
-
-// console.log(persons);
-// console.log("-------------");
-// console.log(copiedPersons);
-
-/*
-The return of the commands above returns Michael in persons and copiedPersons array. This happens because we copied the address to the place in memory with the copiedPersons variable we created. We then changed the value of our name key in the memory itself. Any changes we make to either of the arrays will be reflected in its counterpart.
- */
-
-let mappedPersons = persons.map (person => ({
-    name: person.name,
-    age: person.age
-}));
-mappedPersons[0].name = "Zachary";
-
-// console.log(persons);
-// console.log("-------------");
-// console.log(mappedPersons);
-
-/*
-Only copy what you plan on changing, and where you don't want those changes reflected in the original set of data.
-*/
-
-// Array Destructuring Continued
+// Spread operator pulls out all array elements and returns as a standalone list of elements.
+// */
 
 // let fullName = ["Paul", "Niemcyzk"];
 
-// let firstName = fullName[0];
+// let unpackedFullName = [...fullName];
+
+// // for (name of fullName) {
+// //     let nameNew = []
+// //     nameNew.push(name)
+// //     return nameNew
+// // }
+
+// // console.log(unpackedFullName);
+
+// // let editedFullName = [fullName];
+// // console.log(editedFullName);
+
+// // fullName.unshift("Mr.")
+
+// // console.log(fullName, unpackedFullName);
+
+// // console.log(Math.min(1, 5, -3));
+
+// let prices = [10.99, 5.99, 22.99, 6.88]
+// // console.log(Math.min(prices));
+// // console.log(typeof prices);
+// // console.log(Math.min(prices));// errors out because Math.min requires integers to function
+// // console.log(Math.min(...prices)); // unpacks our array values
+
+// // Primitive data types stored inside memory as a variable and a value
+// let x = 10;
+// let y = "abc";
+// let a = x;
+// let b = y; 
+// // console.log(x, y, a, b);
+
+// // JS has 3 data types that are objects which reference to a value:
+// /*
+//     - array
+//     - function
+//     - object    
+// */
+
+// let arr = [ ];
+// // console.log(arr);
+
+// arr.push(1);
+// // console.log(arr);
+
+// let reference = [1];
+// let refCopy = reference;
+// // console.log(refCopy);
+
+// let persons = [{name: "Zach", age: 28}, {name: "Donovan", age: 30}];
+
+// let copiedPersons = [...persons];
+
+// // console.log(persons);
+// // console.log("-------------");
+// // console.log(copiedPersons);
+
+// persons.push({name: "Anna", age: 29});
+
+// // console.log(persons);
+// // console.log("-------------");
+// // console.log(copiedPersons);
+
+// copiedPersons[0].name = "Zachary";
+// persons[0].name = "Michael";
+
+// // console.log(persons);
+// // console.log("-------------");
+// // console.log(copiedPersons);
+
+// /*
+// The return of the commands above returns Michael in persons and copiedPersons array. This happens because we copied the address to the place in memory with the copiedPersons variable we created. We then changed the value of our name key in the memory itself. Any changes we make to either of the arrays will be reflected in its counterpart.
+//  */
+
+// let mappedPersons = persons.map (person => ({
+//     name: person.name,
+//     age: person.age
+// }));
+// mappedPersons[0].name = "Zachary";
+
+// // console.log(persons);
+// // console.log("-------------");
+// // console.log(mappedPersons);
+
+// /*
+// Only copy what you plan on changing, and where you don't want those changes reflected in the original set of data.
+// */
+
+// // Array Destructuring Continued
+
+// // let fullName = ["Paul", "Niemcyzk"];
+
+// // let firstName = fullName[0];
+
+// // console.log(firstName);
+
+// let [firstName, lastName] = fullName;
 
 // console.log(firstName);
+// console.log(lastName);
 
-let [firstName, lastName] = fullName;
+// /*
+// JS pulls values from fullName and assigns them based off their position to the variables we declared using destructuring.
+//  */
 
-console.log(firstName);
-console.log(lastName);
+// // Rest Syntax
 
-/*
-JS pulls values from fullName and assigns them based off their position to the variables we declared using destructuring.
- */
+// /*
+// Utilizes the spread operator (...) to condense values into a single element rather than unpacking them.
+//     - Any values left over from the original object will be packaged inside of it.
+//     - Values can be skipped before packing them, by utilizing commas (,,,...myObject). 
+//     - Rest operator must be the last item of destructured array syntax.
+// */
 
-// Rest Syntax
+// let restArray = ["Paul", "Niemcyzk", "Omit 1", {month: 6, day: 15, year: 1990}, "Omit 2", {sport: "volleyball"}, "Omit 3"];
 
-/*
-Utilizes the spread operator (...) to condense values into a single element rather than unpacking them.
-    - Any values left over from the original object will be packaged inside of it.
-    - Values can be skipped before packing them, by utilizing commas (,,,...myObject). 
-    - Rest operator must be the last item of destructured array syntax.
-*/
+// let [firstName, lastName, , , ,...myObject] = restArray;
 
-let restArray = ["Paul", "Niemcyzk", "Omit 1", {month: 6, day: 15, year: 1990}, "Omit 2", {sport: "volleyball"}, "Omit 3"];
+// console.log(firstName);
+// console.log(lastName);
+// console.log(myObject);
 
-let [firstName, lastName, , , ,...myObject] = restArray;
+// Repl 4.2
 
-console.log(firstName);
-console.log(lastName);
-console.log(myObject);
+let shoppingList = [[ 'eggs', 'milk', 'butter' ], [ 'cleaner', 'trash bags', 'detergent' ], [ 'thank you card', 'pens', 'gift wrapping' ], [ 'shoes', 't-shirt', 'slacks' ]];
+
+let [grocery, cleaning, gifts, clothing] = [...shoppingList]
+
+// console.log(grocery);
+// console.log(cleaning);
+// console.log(gifts);
+// console.log(clothing);
+
+// let unpackedShoppingList = [...shoppingList[0], ...shoppingList[1], ...shoppingList[2], ...shoppingList[3]]
+// console.log(unpackedShoppingList)
+
+//The method below is dependent upon having unpacked the array and reassigning the values first. Essentially unpacking the array into variables first, then zipping each individual item inside each individual array (variable here) back up into one unpackedShoppingList array.
+let unpackedShoppingList = [...grocery, ...cleaning, ...gifts, ...clothing]
+
+let obj = {
+cleaning: [],
+gifts: [],
+clothing: [],
+grocery: []
+};
+
+unpackedShoppingList.map(function(item, index) {
+    // console.log(item, index);
+
+// use else if to make code explicit
+
+    if(index <= 2) {
+        obj.grocery.push(item)
+    } else if (index >=3 && index <= 5) { // or just index <= 5
+        obj.cleaning.push(item) 
+    } else if (index >= 6 && index <= 8) { // or just index <= 8
+        obj.gifts.push(item)
+    } else if (index >= 9) {
+        obj.clothing.push(item)
+    }
+})
+
+console.log(obj);
--- classes.js
+++ classes.js
@@ -27,100 +27,150 @@ Classes considered special functions and therefore can be defined as:
 
 // Class Declaration
 
-class Teacher {
-    // constructor - a class method for creation and initialization of a class object
-    constructor(name, subject, tenure, isPolyglot) {
-        this.name = name;
-        this.subject = subject;
-        this.tenure = tenure;
-        this.isPolyglot = isPolyglot;
-    }
-}
-
-// shows that a Class is just a special function
-// console.log(typeof Teacher);
-
-// instantiates an empty class object
-// console.log(new Teacher());
-
-// let kLockhart = new Teacher("Kate Lockhart", "Coding", 1, true);
-// console.log(kLockhart);
-// kLockhart.name = "Kate Louise Lockhart";
-// console.log(kLockhart);
-
-// Hoisting
-/*
-Function declarations are hoisted. Class declarations are not.
-*/
-
-
-// Class Expression
-
-// let House = class {
-//     constructor(address, bedrooms, bathrooms) {
-//         this.address = address;
-//         this.bedrooms = bedrooms;
-//         this.bathrooms = bathrooms;
-//     }
-    // Method
-//     whatHouse(){
-//         return `Welcome to ${this.address}. Beautiful ${this.bedrooms} bed, ${this.bathrooms} bath house!` 
-//     }
-// };
-// console.log(new House);
-
-// let myHouse = new House("430 N Park Ave", 1, 1);
-// console.log(myHouse);
-// console.log(myHouse.whatHouse());
-
-// let Garage = class extends House {
-//     constructor(address, bedrooms, bathrooms, cars, width, height) {
-//         super(address, bedrooms, bathrooms)
-//         this.cars = cars,
-//         this. width = width;
-//         this.height = height
+// class Teacher {
+//     // constructor - a class method for creation and initialization of a class object
+//     constructor(name, subject, tenure, isPolyglot) {
+//         this.name = name;
+//         this.subject = subject;
+//         this.tenure = tenure;
+//         this.isPolyglot = isPolyglot;
 //     }
 // }
 
-// console.log(new Garage().whatHouse());
+// // shows that a Class is just a special function
+// // console.log(typeof Teacher);
 
-// Challenge
+// // instantiates an empty class object
+// // console.log(new Teacher());
 
-/*
-Create a Car class that takes the make and the model.
-Instantiate new class object named Porsche with same make and model of 911.
-*/
+// // let kLockhart = new Teacher("Kate Lockhart", "Coding", 1, true);
+// // console.log(kLockhart);
+// // kLockhart.name = "Kate Louise Lockhart";
+// // console.log(kLockhart);
 
-class Car {
-    constructor(make, model) {
-        this.make = make;
-        this.model = model;
+// // Hoisting
+// /*
+// Function declarations are hoisted. Class declarations are not.
+// */
+
+
+// // Class Expression
+
+// // let House = class {
+// //     constructor(address, bedrooms, bathrooms) {
+// //         this.address = address;
+// //         this.bedrooms = bedrooms;
+// //         this.bathrooms = bathrooms;
+// //     }
+//     // Method
+// //     whatHouse(){
+// //         return `Welcome to ${this.address}. Beautiful ${this.bedrooms} bed, ${this.bathrooms} bath house!` 
+// //     }
+// // };
+// // console.log(new House);
+
+// // let myHouse = new House("430 N Park Ave", 1, 1);
+// // console.log(myHouse);
+// // console.log(myHouse.whatHouse());
+
+// // let Garage = class extends House {
+// //     constructor(address, bedrooms, bathrooms, cars, width, height) {
+// //         super(address, bedrooms, bathrooms)
+// //         this.cars = cars,
+// //         this. width = width;
+// //         this.height = height
+// //     }
+// // }
+
+// // console.log(new Garage().whatHouse());
+
+// // Challenge
+
+// /*
+// Create a Car class that takes the make and the model.
+// Instantiate new class object named Porsche with same make and model of 911.
+// */
+
+// class Car {
+//     constructor(make, model) {
+//         this.make = make;
+//         this.model = model;
+//     }
+// };
+
+// let Porsche = new Car("Porsche", "911")
+// console.log(Porsche);
+
+// // Challenge
+
+// /*
+// Create an Options class that inherits Car parent function. Give it a property of transmission. Give your 911 a manual or PDK
+// */
+
+// let Options = class extends Car {
+//     constructor(make, model, transmission) {
+//         super (make, model)
+//         this.transmission = transmission;
+//     }
+//     startCar() {
+//         return `Your ${this.model} is running`
+//     }
+// }
+
+
+// let myPorsche = new Options("Porsche", "911", "PDK")
+
+// // Challenge
+// // Modify above class and create startCar method that returns string interpolated result of "Your 911 is running".
+
+// console.log(myPorsche.startCar());
+
+
+// Repl 4.1
+// default values go into constructor for ES6 and above
+
+class CapsuleContender {  
+    constructor(fuel, maxFuel = 3000, dockingRing = false, radarSystem = false, reactionControlSystem = false) { 
+        this.fuel = fuel;
+        this.maxFuel = maxFuel;
+        this.dockingRing = dockingRing;
+        this.radarSystem = radarSystem;
+        this.reactionControlSystem = reactionControlSystem;
+    } 
+    currentFuelPercentage() {
+        return (this.fuel / this.maxFuel) * 100
     }
-};
-
-let Porsche = new Car("Porsche", "911")
-console.log(Porsche);
-
-// Challenge
-
-/*
-Create an Options class that inherits Car parent function. Give it a property of transmission. Give your 911 a manual or PDK
-*/
-
-let Options = class extends Car {
-    constructor(make, model, transmission) {
-        super (make, model)
-        this.transmission = transmission;
+    toggleDockingRing(toggle) {
+        this.dockingRing = toggle;
     }
-    startCar() {
-        return `Your ${this.model} is running`
+    toggleRadar(toggle) {
+        this.radarSystem = toggle;
+    }
+
+    toggleRCS(toggle) {
+        if (toggle = true && this.currentFuelPercentage() >= 40) {
+            this.reactionControlSystem = toggle;
+        } else {
+            console.log("Insufficent fuel to perform procedure.")
+        }
+    }
+    preDockingProcedure() {
+        if (this.dockingRing && this.radarSystem && this.reactionControlSystem) {
+            return 200
+        } else {
+            return 500
+        }
     }
 }
 
+let spaceship = new CapsuleContender(2000)
 
-let myPorsche = new Options("Porsche", "911", "PDK")
+// console.log(spaceship)
 
-// Challenge
-// Modify above class and create startCar method that returns string interpolated result of "Your 911 is running".
+// spaceship.toggleDockingRing(true)
 
-console.log(myPorsche.startCar());
+spaceship.toggleRCS(true)
+spaceship.toggleDockingRing(true)
+spaceship.toggleRadar(true)
+console.log(spaceship.preDockingProcedure());
+console.log(spaceship)
